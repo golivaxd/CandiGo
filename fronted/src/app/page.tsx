206b7d30f1e18c63f2faed17912f7ff6c000e63f
Lizette Vazquez <lizette.vazquez6156@alumnos.udg.mx>
2025-08-14 00:29:10 -0600

    Subiendooooooo222

--- fronted/src/app/page.tsx
+++ fronted/src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import { supabase } from '@/lib/supabaseClient';
 import { useRouter } from 'next/navigation';
+import '/CSS/Login.css';
 
 export default function Home() {
   const [email, setEmail] = useState('');
