aa280e64df1516d92ba57db494ef6824dac04b99
Goliva <95060883+golivaxd@users.noreply.github.com>
2025-10-02 10:37:29 -0600

    Actualizar page.tsx

--- fronted/src/app/nosotros/page.tsx
+++ fronted/src/app/nosotros/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import './CSS/nosotros.css';
+import './css/nosotros.css';
 
 const Nosotros: React.FC = () => {
   const router = useRouter();
